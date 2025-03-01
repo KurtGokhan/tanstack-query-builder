fd54d48e8503f706fa1c33f6a399c5723e43575e
Gokhan Kurt <krtgkn@gmail.com>
2025-03-01 18:07:52 +0300

    improve example mocks

---
+++ examples/vite/src/mocks/index.ts
@@ -0,0 +1,3 @@
+export * from './mocks';
+export * from './posts';
+export * from './comments';
--- examples/vite/src/mocks/mocks.ts
+++ examples/vite/src/mocks/mocks.ts
@@ -1,9 +1,9 @@
 import { http, HttpResponse, delay } from 'msw';
 import { setupWorker } from 'msw/browser';
+import { mockComments } from './comments';
+import { PostData, mockPosts } from './posts';
 
 export type UserData = { id: number; name: string; email: string; username: string; website: string };
-export type PostData = { id: number; title: string; body: string; userId: number };
-export type CommentData = { postId: number; id: number; name: string; email: string; body: string };
 
 export const baseUrl = 'https://mock.blog.example.com';
 
@@ -18,85 +18,7 @@ function createMockData() {
     },
   ];
 
-  const posts: PostData[] = [
-    {
-      id: 7,
-      userId: 0,
-      title: 'Exploring the Future of AI',
-      body: 'Artificial intelligence is transforming industries, from healthcare to finance. This post explores the latest advancements and future possibilities.',
-    },
-    {
-      id: 1,
-      userId: 0,
-      title: '10 Tips for Better Time Management',
-      body: 'Managing your time effectively can boost productivity and reduce stress. Here are ten practical tips to help you stay on top of your tasks.',
-    },
-    {
-      id: 2,
-      userId: 0,
-      title: "A Beginner's Guide to Gardening",
-      body: 'Starting a garden can be both fun and rewarding. This guide covers the basics, from choosing plants to maintaining healthy soil.',
-    },
-    {
-      id: 3,
-      userId: 0,
-      title: 'Understanding Cryptocurrency',
-      body: 'Cryptocurrency has gained popularity as an alternative form of investment. Learn how it works, its risks, and potential rewards.',
-    },
-    {
-      id: 4,
-      userId: 0,
-      title: 'Top 5 Travel Destinations for 2025',
-      body: 'Looking for your next adventure? Here are five must-visit destinations for 2025, each offering unique experiences and breathtaking views.',
-    },
-  ];
-
-  const comments: CommentData[] = [
-    {
-      id: 1,
-      postId: 3,
-      name: 'Mark Rivera',
-      email: 'mark.rivera@example.com',
-      body: "Very informative! I'd love to read more about crypto security practices.",
-    },
-    {
-      id: 2,
-      postId: 3,
-      name: 'Ella Johnson',
-      email: 'ella.johnson@example.com',
-      body: 'Crypto can be tricky for beginners. Thanks for breaking it down!',
-    },
-    {
-      id: 3,
-      postId: 3,
-      name: 'Ryan Clark',
-      email: 'ryan.clark@example.com',
-      body: 'Is there a specific exchange you recommend for beginners?',
-    },
-    {
-      id: 4,
-      postId: 0,
-      name: 'Jane Doe',
-      email: 'jane.doe@example.com',
-      body: 'Great insights! I wonder how AI will shape education in the next decade.',
-    },
-    {
-      id: 5,
-      postId: 0,
-      name: 'Liam Smith',
-      email: 'liam.smith@example.com',
-      body: 'AI in healthcare is fascinating. The potential for early diagnosis is promising.',
-    },
-    {
-      id: 6,
-      postId: 2,
-      name: 'Sophia Lee',
-      email: 'sophia.lee@example.com',
-      body: 'Thanks for the tips! Do you recommend any beginner-friendly plants for small spaces?',
-    },
-  ];
-
-  return { users, posts, comments };
+  return { users, posts: mockPosts, comments: mockComments };
 }
 
 function saveMockData(saveData = { users, posts, comments }) {
@@ -167,7 +89,7 @@ const handlers = [
   http.delete(`${baseUrl}/posts/:id`, async (req) => {
     await delay(1000);
     const { id } = req.params;
-    if (id === '7' && Math.random() > 0.1) return HttpResponse.json({ error: '[Mock] Failed to delete' }, { status: 500 });
+    if (id === '0') return HttpResponse.json({ error: '[Mock] Failed to delete' }, { status: 500 });
 
     const postIndex = posts.findIndex((post) => post.id === Number(id));
     if (postIndex === -1) return HttpResponse.json({ error: 'Not found' }, { status: 404 });
